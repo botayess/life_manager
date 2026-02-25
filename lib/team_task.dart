fc36a05752f2fce692c92bc853710ee68e218fab
Erasyl77 <8erasyl8@gmail.com>
2026-02-25 12:23:27 +0500

    Add showMenu function

--- lib/team_task.dart
+++ lib/team_task.dart
@@ -2,3 +2,14 @@ void main() {
   print("Life Manager project started!");
   print("Hello from Participant 1");
 }
+
+void showMenu() {
+  print("1. Tasks");
+  print("2. Goals");
+  print("3. Notes");
+}
+
+void main() {
+  print("Life Manager project started!");
+  showMenu();
+}
